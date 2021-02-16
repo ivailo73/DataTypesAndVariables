1a37f94b5b63b1712c427464dc5f35ade861135f
Ivaylo Ivanov <ivailo73@abv.bg>
2021-02-16 04:28:38 +0200

    Add project files.

---
+++ 03.ExactSumOfRealNumbers/ExactSumOfRealNumbers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.ExactSumOfRealNumbers
+{
+    class ExactSumOfRealNumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DataTypesAndVariables/MeterToKilometer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DataTypesAndVariables
+{
+    class MeterToKilometer
+    {
+        static void Main(string[] args)
+        {
+            int meter = int.Parse(Console.ReadLine());
+            decimal kilometer = meter / 1000.0m;
+            Console.WriteLine($"{kilometer:f2}");
+        }
+    }
+}
